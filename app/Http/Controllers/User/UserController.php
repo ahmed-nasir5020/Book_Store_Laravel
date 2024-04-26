aa4494d001b3bf4a2ba3714c3fc552b097b003ee
ahmed-nasir5020 <an0991382813@gmail.com>
2024-04-26 12:36:49 +0300

    create_book_contoller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Egulias\EmailValidator\Result\Reason\Reason;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // show all book
+    public function index():Response{
+     
+     return response()->json([]);
+    }
 }
